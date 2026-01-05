de4f59a70a8facbf28a94cbc623e7232eeb1aa2c
shenron221B <120735073+shenron221B@users.noreply.github.com>
2026-01-05 16:48:18 +0100

    fix: aggiunto header per le classi di test generate con LLM

--- core/spring/src/test/java/org/apache/syncope/core/spring/policy/DefaultPasswordRuleZeroShotLLMTest.java
+++ core/spring/src/test/java/org/apache/syncope/core/spring/policy/DefaultPasswordRuleZeroShotLLMTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.syncope.core.spring.policy;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -45,10 +64,6 @@ class DefaultPasswordRuleZeroShotLLMTest {
         validConf.getWordsNotPermitted().add("forbidden");
     }
 
-    // ---------------------------------------------------------------------
-    // setConf tests
-    // ---------------------------------------------------------------------
-
     @Test
     void setConf_shouldAcceptDefaultPasswordRuleConf() {
         assertDoesNotThrow(() -> rule.setConf(validConf));
@@ -66,10 +81,6 @@ class DefaultPasswordRuleZeroShotLLMTest {
 
         assertTrue(ex.getMessage().contains(DefaultPasswordRuleConf.class.getName()));
     }
-
-    // ---------------------------------------------------------------------
-    // enforce(String, String) tests
-    // ---------------------------------------------------------------------
 
     @Test
     void enforce_shouldDoNothing_whenPasswordIsNull() {
